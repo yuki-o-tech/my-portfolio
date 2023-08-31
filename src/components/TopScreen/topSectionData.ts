6628e55b06dbeec99de4820ca17c5542f54d009b
Yuki Otsubo <yuki.otsubo.tech@gmail.com>
2023-08-31 15:10:22 +0900

    add Animation and Edit the text

--- src/components/TopScreen/topSectionData.ts
+++ src/components/TopScreen/topSectionData.ts
@@ -8,5 +8,5 @@ export const topSection: TopSectionProps = {
   title: "Welcome to my portfolio!",
   subTitle: "Frontend Development | Aspiring UI/UX Design",
   description:
-    "こんにちは、Yukiです。直感的で魅力的なユーザー体験の創造に情熱を注ぐフロントエンドエンジニアとして、確かなコーディングスキルとデザインに対する鋭い目を兼ね備えています。",
+    "こんにちは、Yukiです。直感的で魅力的なユーザー体験の創造に情熱を注ぐフロントエンドエンジニアとして常に挑戦し続けています。",
 }
